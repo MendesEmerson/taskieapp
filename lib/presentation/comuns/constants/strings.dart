372c95d55e58729e280c7ba8c30f999134144ebd
C1337001 Emerson Mendes da Costa <c1337001@interno.bb.com.br>
2024-06-06 17:43:45 -0300

    FEAT: Adicionando classes de utilização comum

---
+++ lib/presentation/comuns/constants/strings.dart
@@ -0,0 +1,8 @@
+class StringsConstants {
+  static const login = "Login";
+  static const cadastro = "Cadastro";
+  static const textHomePage = "Não deixe para fazer amanhã o que você pode fazer hoje com Taskie.";
+  static const organize = "Organize.";
+  static const simplifique = "Simplifique.";
+  static const conquiste = "Conquiste.";
+}
---
+++ lib/presentation/comuns/styles/custom_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const textColor = Colors.white60;
+  static const titleTextColor = Colors.white;
+  static const roxoPadrao = Color(0xFFDDB7FB);
+  static const padraoWidgetTransparente = Color(0x41ECECEC);
+  static const backgroundAlertDialog = Color(0xDD000000);
+
+}
