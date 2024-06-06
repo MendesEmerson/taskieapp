bb9154f4bb9764c118edc444b19caf7a265f7762
C1337001 Emerson Mendes da Costa <c1337001@interno.bb.com.br>
2024-06-06 19:28:45 -0300

    FEAT: Criação da classe de injeção de dependências

---
+++ lib/di/service_locator.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+
+import '../data/repositories/firebase_auth_repository_impl.dart';
+import '../domain/repositories/firebase_auth_repository.dart';
+
+final GetIt getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+
+  getIt.registerLazySingleton<FirebaseAuthRepository>(
+        () => FirebaseAuthRepositoryImpl(getIt<FirebaseAuth>()),
+  );
+}
